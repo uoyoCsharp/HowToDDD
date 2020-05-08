16c1e8dc41465533b3c37595fc3917c3aebb6100
uoyo <32832304+uoyoCsharp@users.noreply.github.com>
2020-05-08 10:09:49 +0800

    迁移工作单元demo到该仓库

---
+++ UnitOfWorkDemo/EasyUowApplication/Repositories/ItineraryRepository.cs
@@ -0,0 +1,20 @@
+﻿using EasyUowApplication.Aggregates;
+using EasyUowApplication.EFCore;
+using MiCake.Uow.Easy;
+using System;
+using MiCake.EFCore.Easy;
+
+namespace EasyUowApplication.Repositories
+{
+    public class ItineraryRepository : EFRepository<UowAppDbContext, Itinerary, Guid>
+    {
+        public ItineraryRepository(IUnitOfWorkManager uowManager) : base(uowManager)
+        {
+        }
+
+        public void Add(Itinerary itinerary)
+        {
+            DbContext.Set<Itinerary>().Add(itinerary);
+        }
+    }
+}
---
+++ UnitOfWorkDemo/MiCake.Uow.Easy/MiCakeUowEasyModule.cs
@@ -0,0 +1,23 @@
+﻿using MiCake.Core.Abstractions.Modularity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiCake.Uow.Easy
+{
+    public class MiCakeUowEasyModule : MiCakeModule
+    {
+        public MiCakeUowEasyModule()
+        {
+        }
+
+
+        //添加需要所需要的注入服务
+        public override void ConfigServices(ModuleConfigServiceContext context)
+        {
+            context.Services.AddSingleton<IUnitOfWorkManager, UnitOfWorkManager>();
+            context.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
